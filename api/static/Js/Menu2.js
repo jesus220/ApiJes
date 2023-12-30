9fd58369f72c79510010d9124c721767c00e132d
Alfonso2208 <jesus490a@gmail.com>
2023-12-30 13:58:53 -0600

    new commit 1.10

--- api/static/Js/Menu2.js
+++ api/static/Js/Menu2.js
@@ -1,34 +1,18 @@
+// by: George Olaru https://dribbble.com/shots/1560982-Rosa-Restaurant-Website/attachments/239212
+
 $(document).ready(function () {
-  var zindex = 10;
-
-  $("div.card").click(function (e) {
-    e.preventDefault();
-
-    var isShowing = false;
-
-    if ($(this).hasClass("show")) {
-      isShowing = true;
+  $("button").on("click", function () {
+    if ($(this).hasClass("nav-button")) {
+      $("nav div").addClass("show");
+    } else if ($(this).hasClass("exit-menu")) {
+      $("nav div").removeClass("show");
+    } else if ($(this).hasClass("to-top")) {
+      $("html,body").animate({ scrollTop: 0 }, "slow");
     }
+  });
 
-    if ($("div.cards").hasClass("showing")) {
-      // a card is already in view
-      $("div.card.show").removeClass("show");
-
-      if (isShowing) {
-        // this card was showing - reset the grid
-        $("div.cards").removeClass("showing");
-      } else {
-        // this card isn't showing - get in with it
-        $(this).css({ zIndex: zindex }).addClass("show");
-      }
-
-      zindex++;
-    } else {
-      // no cards in view
-      $("div.cards").addClass("showing");
-      $(this).css({ zIndex: zindex }).addClass("show");
-
-      zindex++;
-    }
+  AOS.init({
+    duration: 1800,
+    easing: "ease"
   });
 });
